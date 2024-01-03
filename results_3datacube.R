8914ab3537968ddf7b964ba4bdf5b58c8e8b1025
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2024-01-03 14:50:34 -0800

    Moved QC code to parking lot; changed RFFC to Hybrid; made sure to retain 'run' id; notes on where to pick up again

--- results_3datacube.R
+++ results_3datacube.R
@@ -44,44 +44,43 @@ main <- bind_rows(main_sc, main_nc,
   #sort nicely
   arrange(RRK, HUC12, Priority, TxIntensity, TxType, Year)
 
-# > main %>% pull(HUC12) %>% unique() %>% length()
-# [1] 2837 
-# matches hucs shp
-
-#known explicitly missing: 
-# main %>% filter(is.na(HaCBP))
-# # A tibble: 5 × 13
-# HUC12        RRK   Priority TxIntensity TxType run     Year HaCBP HaCFL hacbp_ci_low hacbp_ci_high hacfl_ci_low hacfl_ci_high
-# <chr>        <chr> <chr>    <chr>       <chr>  <chr>  <dbl> <dbl> <dbl>        <dbl>         <dbl>        <dbl>         <dbl>
-#   1 160501020107 SN    Fire     2m          trt1   RunID9  2024    NA    NA           NA            NA           NA            NA
-# 2 160501020107 SN    Fire     2m          trt6   RunID9  2024    NA    NA           NA            NA           NA            NA
-# 3 180102040102 SN    Fire     1m          trt4   RunID6  2029    NA    NA           NA            NA           NA            NA
-# 4 180102040102 SN    WUI      2m          trt6   RunID7  2029    NA    NA           NA            NA           NA            NA
-# 5 180200021403 SN    Fire     2m          trt4   RunID9  2029    NA    NA           NA            NA           NA            NA
-
-# > nrow(main)
-# [1] 299765
-
-#unknown implicitly missing???
-# > 2837*108
-# [1] 306396
-# > 2837*108-nrow(main)
-# [1] 6631
 
 main_expand <- main %>% 
   #NOT runID b/c can't associate it to huc/region/priority/intensity/type without losing some combinations
+  # will add back in later
   expand(nesting(HUC12, RRK), Priority, TxIntensity, TxType, Year)
 
 main_full <- main_expand %>% 
-  left_join(main, by = c("HUC12", "RRK", "Priority", "TxIntensity", "TxType", "Year"))
+  left_join(main, by = c("HUC12", "RRK", "Priority", "TxIntensity", "TxType", "Year")) %>% 
+  mutate(missing_flag = ifelse(is.na(run), TRUE, FALSE))
 
-# > main_full %>% filter(is.na(run)) %>% pull(RRK) %>% unique()
-# [1] "NC" "SN" #6,631
-# Checked a couple - one was missing a whole year folder, another missing just that scenario-year folder
 
-# main_full %>% filter(is.na(run)) %>% 
-#   select(HUC12, RRK, Priority, TxIntensity, TxType, Year) %>% 
-#   write_csv(file.path(output_folder, 'missing_main_20230101.csv')) 
+# Make crosswalk that contains run, so can add run back in here
+run_xwalk <- main %>% 
+  select(HUC12, RRK, Priority, TxIntensity, run) %>% 
+  distinct() %>% 
+  rename(run_update = run)
+
+#add run back in for NA (rows that had been missing)
+main_full <- main_full %>% 
+  left_join(run_xwalk, by = join_by(HUC12, RRK, Priority, TxIntensity)) %>% 
+  mutate(run = ifelse(missing_flag, run_update, run))
+  
+
+#with full scenario run ID
+# for val, offshoot
+# main_full %>% 
+#   filter(missing_flag) %>%
+#   mutate(mas_scenario = paste(run, RRK, Priority, TxIntensity, TxType, sep="_")) %>% 
+#   select(HUC12, mas_scenario, run, Year, RRK, Priority, TxIntensity, TxType) %>% 
+#   arrange(RRK, HUC12, mas_scenario) %>% 
+#   write_csv(file.path(output_folder, 'missing_main_20230103.csv'))
+
+
+#remove missing_flag, select(-run_update)
+main_full <- main_full %>% 
+  select(-missing_flag, -run_update)
+  
 
 ### Fire type Data import & prep -------------------------------------------
 
@@ -112,27 +111,11 @@ ft <- bind_rows(ft_sc, ft_nc,
   #sort nicely
   arrange(RRK, HUC12, Priority, TxIntensity, TxType, Year)
 
-# > ft %>% filter(is.na(fire_type))
-# # A tibble: 17 × 9
-# HUC12        RRK   Priority TxIntensity TxType run      Year fire_type burn_frac_ave
-# <chr>        <chr> <chr>    <chr>       <chr>  <chr>   <dbl>     <dbl>         <dbl>
-#   1 180500060302 CC    RFFC     2m          trt4   RunID35  2034        NA            NA
-# 2 180500060302 CC    WUI      2m          trt4   RunID34  2034        NA            NA
-# 3 180600020503 CC    Fire     1m          trt4   RunID33  2029        NA            NA
-# 4 180703020405 SC    Fire     1m          trt1   RunID24  2029        NA            NA
-# 5 160501020107 SN    Fire     2m          trt1   RunID9   2024        NA            NA
-# 6 160501020107 SN    Fire     2m          trt6   RunID9   2024        NA            NA
-# 7 180102040102 SN    Fire     1m          trt4   RunID6   2029        NA            NA
-# 8 180102040102 SN    WUI      2m          trt6   RunID7   2029        NA            NA
-# 9 180200021101 SN    WUI      2m          trt6   RunID7   2024        NA            NA
-# 10 180200021403 SN    Fire     2m          trt4   RunID9   2029        NA            NA
-# 11 180200030801 SN    Fire     2m          trt4   RunID9   2024        NA            NA
-# 12 180200030801 SN    WUI      1m          trt1   RunID4   2024        NA            NA
-# 13 180300030103 SN    Fire     1m          trt1   RunID6   2024        NA            NA
-# 14 180300030103 SN    Fire     1m          trt6   RunID6   2024        NA            NA
-# 15 180400090202 SN    RFFC     1m          trt1   RunID5   2034        NA            NA
-# 16 180400090202 SN    RFFC     2m          trt1   RunID8   2034        NA            NA
-# 17 180800030302 SN    RFFC     500k        trt4   RunID2   2024        NA            NA
+
+
+
+
+
 
 
 #Anna's FVS results
@@ -143,18 +126,36 @@ fvs <- read_csv(file.path(input_folder,
 # > nrow(fvs)
 # [1] 321624 ??
 
-fvs_dupls <- fvs %>% group_by(HUC12, Priority, TxIntensity, TxType, Year) %>% filter(n()>1) %>% summarize(n=n()) %>% ungroup()
-
-fvs_dupls %>% 
-  left_join(fvs, by = c("HUC12", "Priority", "TxIntensity", "TxType", "Year")) #%>% View()
-
-fvs_dupls %>% 
-  left_join(fvs, by = c("HUC12", "Priority", "TxIntensity", "TxType", "Year")) %>% 
-  select(HUC12, RRK) %>% 
-  distinct()
+# fvs_dupls <- fvs %>% group_by(HUC12, Priority, TxIntensity, TxType, Year) %>% filter(n()>1) %>% summarize(n=n()) %>% ungroup()
+# 
+# fvs_dupls %>% 
+#   left_join(fvs, by = c("HUC12", "Priority", "TxIntensity", "TxType", "Year")) #%>% View()
+# 
+# fvs_dupls %>% 
+#   left_join(fvs, by = c("HUC12", "Priority", "TxIntensity", "TxType", "Year")) %>% 
+#   select(HUC12, RRK) %>% 
+#   distinct()
 
 
 ### Data processing ------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #pivot fire type results from longer to 
 ft_wide <- ft %>% 
@@ -189,6 +190,11 @@ res_all <- fvs %>%
             by = c("HUC12", "RRK", "Priority", "TxIntensity", "TxType", "Year"))
   
 
+#rename Priority RFFC to Hybrid
+res_all <- res_all %>% 
+  #change name to Hybrid
+  mutate(Priority = ifelse(Priority == 'RFFC', 'Hybrid', Priority))
+  
 
 ### Save out ------------------------------------------
 
@@ -206,3 +212,61 @@ res_all <- fvs %>%
 write_csv(res_all, 
           file.path(output_folder, 
                     paste0('datacube_20230102a.csv')))
+
+
+
+### PArking lot -----------------------------------------
+
+# > main %>% pull(HUC12) %>% unique() %>% length()
+# [1] 2837 
+# matches hucs shp
+
+#known explicitly missing: 
+# main %>% filter(is.na(HaCBP))
+# # A tibble: 5 × 13
+# HUC12        RRK   Priority TxIntensity TxType run     Year HaCBP HaCFL hacbp_ci_low hacbp_ci_high hacfl_ci_low hacfl_ci_high
+# <chr>        <chr> <chr>    <chr>       <chr>  <chr>  <dbl> <dbl> <dbl>        <dbl>         <dbl>        <dbl>         <dbl>
+#   1 160501020107 SN    Fire     2m          trt1   RunID9  2024    NA    NA           NA            NA           NA            NA
+# 2 160501020107 SN    Fire     2m          trt6   RunID9  2024    NA    NA           NA            NA           NA            NA
+# 3 180102040102 SN    Fire     1m          trt4   RunID6  2029    NA    NA           NA            NA           NA            NA
+# 4 180102040102 SN    WUI      2m          trt6   RunID7  2029    NA    NA           NA            NA           NA            NA
+# 5 180200021403 SN    Fire     2m          trt4   RunID9  2029    NA    NA           NA            NA           NA            NA
+
+# > nrow(main)
+# [1] 299765
+
+#unknown implicitly missing???
+# > 2837*108
+# [1] 306396
+# > 2837*108-nrow(main)
+# [1] 6631
+
+# > main_full %>% filter(is.na(run)) %>% pull(RRK) %>% unique()
+# [1] "NC" "SN" #6,631
+# Checked a couple - one was missing a whole year folder, another missing just that scenario-year folder
+
+# main_full %>% filter(is.na(run)) %>% 
+#   select(HUC12, RRK, Priority, TxIntensity, TxType, Year) %>% 
+#   write_csv(file.path(output_folder, 'missing_main_20230101.csv')) 
+
+# > ft %>% filter(is.na(fire_type))
+# # A tibble: 17 × 9
+# HUC12        RRK   Priority TxIntensity TxType run      Year fire_type burn_frac_ave
+# <chr>        <chr> <chr>    <chr>       <chr>  <chr>   <dbl>     <dbl>         <dbl>
+#   1 180500060302 CC    RFFC     2m          trt4   RunID35  2034        NA            NA
+# 2 180500060302 CC    WUI      2m          trt4   RunID34  2034        NA            NA
+# 3 180600020503 CC    Fire     1m          trt4   RunID33  2029        NA            NA
+# 4 180703020405 SC    Fire     1m          trt1   RunID24  2029        NA            NA
+# 5 160501020107 SN    Fire     2m          trt1   RunID9   2024        NA            NA
+# 6 160501020107 SN    Fire     2m          trt6   RunID9   2024        NA            NA
+# 7 180102040102 SN    Fire     1m          trt4   RunID6   2029        NA            NA
+# 8 180102040102 SN    WUI      2m          trt6   RunID7   2029        NA            NA
+# 9 180200021101 SN    WUI      2m          trt6   RunID7   2024        NA            NA
+# 10 180200021403 SN    Fire     2m          trt4   RunID9   2029        NA            NA
+# 11 180200030801 SN    Fire     2m          trt4   RunID9   2024        NA            NA
+# 12 180200030801 SN    WUI      1m          trt1   RunID4   2024        NA            NA
+# 13 180300030103 SN    Fire     1m          trt1   RunID6   2024        NA            NA
+# 14 180300030103 SN    Fire     1m          trt6   RunID6   2024        NA            NA
+# 15 180400090202 SN    RFFC     1m          trt1   RunID5   2034        NA            NA
+# 16 180400090202 SN    RFFC     2m          trt1   RunID8   2034        NA            NA
+# 17 180800030302 SN    RFFC     500k        trt4   RunID2   2024        NA            NA
