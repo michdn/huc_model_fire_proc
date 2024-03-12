545c1c018ecc9f86bcdfeaf738a757ff54f53dae
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2024-03-12 12:59:20 -0700

    Constructing folders rather than just grepping

--- scripts/chickadee_transfer/transfer_chickadee_laptop.R
+++ scripts/chickadee_transfer/transfer_chickadee_laptop.R
@@ -1,21 +1,30 @@
 # for transferring blended fuels from chickadee to local laptop for qaqc
 # (used to be for production, but now leaving on bluejay with processing on bluejay)
 
-# Mostly looking at fml. 
+# Mostly looking at fml or canopy cover cc. 
+
+
+# ***********
+# USE V2 NOW! 
+# **********
+
+# Too many folders now, with gridfire in same shared directory,
+# initial dir scan was taking ~hour
+
+
+
 
 ### User settings ---------------------------------------------
 
 #Mounted shared: R:\rem
 shared <- file.path("R:", "rem")
-#test <- file.path("R:", "rem", "RunID19_SC_WUI_500k_trt1")
-# update 2024-03 new blended rasters
-# New blended FM40 rasters (hopefully correct): /mnt/share/rem/RunID21_SC_Fire_500k_trt6/FVS_fuels/blended_outputs/*fml*tif
-# Original blended FM40 rasters (incorrect): /mnt/share/rem/RunID21_SC_Fire_500k_trt6_orig/FVS_fuels/blended_outputs/*fml*tif
 
-
-# run ids, include _, important for single digit runids
+# run ids, include _ if runid only, important for single digit runids
 selected_runs <- 
-  "RunID21_|RunID27_|RunID26_|RunID20_|RunID25_"
+  'RunID30_.+trt6|RunID32_.+trt6|RunID33_.+trt1|RunID34_.+trt1|RunID35_.+trt4|RunID36_.+trt4' #CC
+  #'RunID21_.+trt1|RunID25_.+trt1|RunID23_.+trt1' # trt4 # part of SC
+  #'RunID25_|RunID20_ #trt1 #part of SC
+  #"RunID20_|RunID21_|RunID23_|RunID24_|RunID26_|RunID27_" # part of SC
 
 
 #My drive & folder over RDC: 
@@ -23,7 +32,7 @@ outfolder <- file.path(
   #backslashes escaped
   "\\\\tsclient", 
   "C", "Users", "nekodawn", "code_local", 
-  "huc_model_fire", "qaqc_fvs", "sc_qaqc") #sc_qaqc, cc_qaqc, nc_qaqc, sn_qaqc
+  "huc_model_fire", "qaqc_fvs", "cc_qaqc") #sc_qaqc, cc_qaqc, nc_qaqc, sn_qaqc
 
 
 ### select files -----------------------------------------------------
@@ -31,40 +40,47 @@ outfolder <- file.path(
 # Plan is to get all directories, then filter to correct directories
 # Then copy files over
 
+#This will take a while
 all_dirs <- list.dirs(shared) 
 
 
-#Run per region   
-region_dirs <- grep(pattern="SC", all_dirs, value=TRUE)
-#just FVS_fuels
-fvsfuels_dirs <- grep(pattern="FVS_fuels", region_dirs, value=TRUE)
-#just blended fuels
-blended_dirs <- grep(pattern="blended_outputs", fvsfuels_dirs, value=TRUE)
-
+# #Run per region   (not really needed now with RunID pattern, but doesn't really hurt)
+# region_dirs <- grep(pattern="SC", all_dirs, value=TRUE)
 
 #test scenarios
-test_folders <- grep(pattern=selected_runs, blended_dirs, value=TRUE)
-#need to get rid of '_orig' folders
-orig_removed <- grep(pattern='_orig', test_folders, invert=TRUE, value=TRUE)
-#need to get rid of '_wrong' folders
-wrong_removed <- grep(pattern='_wrong', orig_removed, invert=TRUE, value=TRUE)
+test_folders <- grep(pattern=selected_runs, all_dirs, value=TRUE)
 
+#just FVS_fuels and blended
+fuels_dirs <- grep(pattern="/FVS_fuels/blended_outputs$", test_folders, value=TRUE)
+fuels_dirs
 
+#need to get rid of various folders (may not need, no harm to include)
+do_not_find <- '_orig|_wrong|_re-run|MAS_delete|MAS_orig|_moved'
+dnf_removed <- grep(pattern=do_not_find, fuels_dirs, invert=TRUE, value=TRUE)
 
-tif_files <- list.files(wrong_removed,
+dnf_removed
+
+fml_files <- list.files(dnf_removed,
                         full.names = TRUE,
-                        #no longer need recursive here, since getting exact
-                        # folders from above
-                        #recursive = TRUE,
-                        #probably only needed "tif$" but extra pattern shouldn't hurt
-                        pattern = "RunID.+_fml_.+tif$") #"RunID.+_fml_.+tif$"
+                        pattern = "RunID.+_fml_.+tif$") 
 
+cc_files <- list.files(dnf_removed,
+                        full.names = TRUE,
+                        pattern = "RunID.+_cc_.+tif$") 
 
+fml_files
+cc_files
+
+# Need to REMOVE 2044
+fml_files <- grep(fml_files, pattern="_2044_", invert=TRUE, value=TRUE)
+cc_files <- grep(cc_files, pattern="_2044_", invert=TRUE, value=TRUE)
 
 ### copy files --------------------------------------------------------
 (time_start <- Sys.time())
 
-file.copy(from=tif_files, to=outfolder)
+file.copy(from=fml_files, to=outfolder)
+file.copy(from=cc_files, to=outfolder)
+
 
 (time_end <- Sys.time())
 (time_end - time_start) 
---
+++ scripts/chickadee_transfer/transfer_chickadee_laptop_v2.R
@@ -0,0 +1,96 @@
+# for transferring blended fuels from chickadee to local laptop for qaqc
+# (used to be for production, but now leaving on bluejay with processing on bluejay)
+
+# Mostly looking at fml or canopy cover cc. 
+
+### Libraries -------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  tidyverse) 
+
+
+### User settings ---------------------------------------------
+
+#Mounted shared: R:\rem
+shared <- file.path("R:", "rem")
+
+# run ids, include _ if runid only, important for single digit runids
+selected_runs <- 
+  'RunID30_.+trt6|RunID32_.+trt6|RunID33_.+trt1|RunID34_.+trt1|RunID35_.+trt4|RunID36_.+trt4' #CC
+  #'RunID21_.+trt1|RunID25_.+trt1|RunID23_.+trt1' # trt4 # part of SC
+  #'RunID25_|RunID20_ #trt1 #part of SC
+  #"RunID20_|RunID21_|RunID23_|RunID24_|RunID26_|RunID27_" # part of SC
+
+
+#My drive & folder over RDC: 
+outfolder <- file.path(
+  #backslashes escaped
+  "\\\\tsclient", 
+  "C", "Users", "nekodawn", "code_local", 
+  "huc_model_fire", "qaqc_fvs", "cc_qaqc") #sc_qaqc, cc_qaqc, nc_qaqc, sn_qaqc
+
+
+fvs_names <- read_csv(file.path("data", "FvsNames.csv"))
+
+
+### select files -----------------------------------------------------
+
+#create folders that look like R:/rem/RunID36_CC_Fire_2m_trt4/FVS_fuels/blended_outputs/
+
+fvs_names <- fvs_names %>% 
+  mutate(runid_folders = file.path('R:',
+                                   'rem',
+                                   name,
+                                   'FVS_fuels',
+                                   'blended_outputs'))
+
+
+selected <- grep(pattern=selected_runs, 
+                 fvs_names %>% pull(runid_folders),
+                 value=TRUE)
+  
+
+fml_files <- list.files(selected,
+                        full.names = TRUE,
+                        pattern = "RunID.+_fml_.+tif$") 
+
+cc_files <- list.files(selected,
+                       full.names = TRUE,
+                       pattern = "RunID.+_cc_.+tif$") 
+
+
+# Need to REMOVE 2044
+fml_files <- grep(fml_files, pattern="_2044_", invert=TRUE, value=TRUE)
+cc_files <- grep(cc_files, pattern="_2044_", invert=TRUE, value=TRUE)
+
+
+
+
+# No pattern parameter in list.dirs unlike list.files
+# Plan is to get all directories, then filter to correct directories
+# Then copy files over
+#This will take a while -- TOO MANY FOLDERS NOW
+# all_dirs <- list.dirs(shared) 
+# #Run per region   (not really needed now with RunID pattern, but doesn't really hurt)
+# region_dirs <- grep(pattern="SC", all_dirs, value=TRUE)
+# #test scenarios
+# test_folders <- grep(pattern=selected_runs, all_dirs, value=TRUE)
+# #just FVS_fuels and blended
+# fuels_dirs <- grep(pattern="/FVS_fuels/blended_outputs$", test_folders, value=TRUE)
+# fuels_dirs
+# #need to get rid of various folders (may not need, no harm to include)
+# do_not_find <- '_orig|_wrong|_re-run|MAS_delete|MAS_orig|_moved'
+# dnf_removed <- grep(pattern=do_not_find, fuels_dirs, invert=TRUE, value=TRUE)
+# dnf_removed
+
+
+### copy files --------------------------------------------------------
+(time_start <- Sys.time())
+
+file.copy(from=fml_files, to=outfolder)
+file.copy(from=cc_files, to=outfolder)
+
+
+(time_end <- Sys.time())
+(time_end - time_start) 
+
