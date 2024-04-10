3b556ac5fe30650faa430cb792d6adb433b9a42b
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2024-04-10 10:44:06 -0700

    Script that corrects the HUC indicator to nonburnable-only for the specific HUCs identified; plus script to pull from existing folders for reruns needing nonburnable updates

---
+++ scripts/gridfire_prep/nonburnable_huc_indicator.R
@@ -0,0 +1,106 @@
+# Script to correct huc_indicator.tif 
+#  To nonburnable area for selected HUCs
+
+
+### Libraries -------------------------------------------------
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  tidyverse,
+  sf, #MUST include b/c of RDS object 
+  terra)
+
+
+### User settings -------------------------------------------
+
+region_to_run <- "SN" # "SC", CC", "SN", "NC"
+
+region_folder <- file.path("E:", "MAS", "gridfire_prep", 
+                           "reruns_nonburn", region_to_run)
+
+
+## FML ------------------------------------------------------
+
+# # fuel model layer to identify nonburnable values
+# fml <- terra::rast("other_datasets/LC22_F40_220_CA.tif")
+# 
+# nonburn <- c(91, 1, 
+#              92, 1, 
+#              93, 1, 
+#              98, 1,
+#              99, 1,
+#              -9999, 1)
+# nonburn <- matrix(nonburn, ncol=2, byrow=TRUE)
+# 
+# #binary 1 = nonburnable, 0 = everything else
+# fml_nb <- terra::classify(fml, 
+#                           nonburn,
+#                           others=0)
+# 
+# #save out to read in without needing to reclass everytime
+# terra::writeRaster(fml_nb, 
+#                    "other_datasets/LC22_F40_220_CA_NBbinary.tif", 
+#                    overwrite=TRUE)
+
+
+## Base data in ----------------------------------------------
+
+fml_nb <- terra::rast("other_datasets/LC22_F40_220_CA_NBbinary.tif")
+
+#plan: subtract this layer from huc_indicator. 
+# huc_indicator has a 1 for the huC, 
+# so nonburnable becomes 1 - 1 = 0. 
+# Good HUC area 1 - 0 = 1
+# and outside of HUC 0 - 0 = 0 or 0 - 1 = -1. 
+# Then classify -1 to 0. 
+
+#hucs to modify, from scripts/qa_misc/hucs_nonburnable.R
+nb_hucs <- readRDS("data/nonburnable_rerun_list.RDS") %>% 
+  filter(region == region_to_run)
+
+
+## Collect huc indicators to modify ------------------------
+
+#nb_hucs_pattern <- nb_hucs %>% pull(huc12) %>% paste(., collapse="|")
+
+target_folders <- nb_hucs %>% 
+  dplyr::select(huc12) %>% 
+  mutate(huc_folder_name = paste0("huc_", huc12),
+         huc_folder = file.path(region_folder, huc_folder_name),
+         target = file.path(huc_folder, "inputs", "huc_indicator.tif"))
+
+
+
+# Few enough, just loop
+
+for (i in 1:nrow(target_folders)){
+  
+  this_target <- target_folders[i,]
+  
+  this_indicator <- rast(this_target[["target"]])
+  
+  #project fuel, using indicator for extent and resolution
+  this_fmlnb <- project(fml_nb, 
+                        this_indicator,
+                        method="near", 
+                        threads=T)
+  
+  #subtract and reclassify -1 back to 0
+  idiff <- this_indicator - this_fmlnb
+  
+  updated <- terra::classify(idiff, 
+                             matrix(c(-1, 0), ncol=2))
+  
+  #save out updated indicator, overwriting
+  writeRaster(updated, this_target[["target"]],
+              overwrite=TRUE)
+  
+  target_folders[i,"update_time"] <- format(Sys.time(), "%Y-%m-%d %H:%M:%S %Z")
+  
+}
+
+
+write_csv(target_folders %>% 
+            dplyr::select(huc12, target, update_time), 
+          file = "log_reruns_indicator_update.csv",
+          append = TRUE)
---
+++ scripts/gridfire_prep/nonburnable_reruns.R
@@ -0,0 +1,66 @@
+# Nonburnable issue
+
+# This pulls out the affected HUCs in SC, CC, SN (previously run)
+
+# Puts them into a new folder
+
+# Then can use the more general nonburnable_huc_indicator.R on these folders
+
+### Libraries -------------------------------------------------
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  tidyverse,
+  sf, #MUST include b/c of RDS object 
+  fs) #for easier directory copy
+
+
+### User settings -------------------------------------------
+
+region_to_run <- "SN" # "SC", CC", "SN"
+
+original_folder <- file.path("E:", "MAS", "gridfire_prep", 
+                           "hucs_gf", 
+                           region_to_run)
+
+new_folder <- file.path("E:", "MAS", "gridfire_prep",
+                            "reruns_nonburn", 
+                            region_to_run)
+
+### Data ----------------------------------------------------
+
+#hucs to collect, from scripts/qa_misc/hucs_nonburnable.R
+nb_hucs <- readRDS("data/nonburnable_rerun_list.RDS") %>% 
+  filter(region == region_to_run)
+
+
+## Collect huc indicators to modify ------------------------
+
+#nb_hucs_pattern <- nb_hucs %>% pull(huc12) %>% paste(., collapse="|")
+
+target_folders <- nb_hucs %>% 
+  dplyr::select(huc12) %>% 
+  mutate(huc_folder_name = paste0("huc_", huc12),
+         huc_folder = file.path(original_folder, huc_folder_name),
+         output_folder = file.path(new_folder, huc_folder_name))
+
+
+for (i in 1:nrow(target_folders)){
+  
+  this_target <- target_folders[i,]
+
+  fs::dir_copy(this_target[["huc_folder"]],
+               this_target[["output_folder"]])
+  
+  
+  #    file.copy(this_file, file.path(this_year_folder, "weather.json"))
+  # # create empty folder Z inside folder X
+  # dir.create('(...)/folder_X/folder_Z')
+  # # copy the folder
+  # file.copy('(...)/path_to_folder_Z', '(...)/folder_X')
+  #https://stackoverflow.com/questions/32453455/copy-folder-recursive-in-r
+}
+
+
+
+
