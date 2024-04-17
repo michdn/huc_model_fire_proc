5bdd083d217304ba699e19b92fc67f5edccfa2dd
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2024-04-17 16:15:42 -0700

    timerseries boxplots and sampled line graphs for FVS results with baseline masquerading as intensity level

---
+++ scripts/results/timeseries_treatmentgroups_baselines_FVS.R
@@ -0,0 +1,274 @@
+# time series graphs
+
+# all HUCs: boxplots
+# sampled HUCs: line graphs
+
+# all regions, all scenarios
+# FVS results! 
+
+# BASELINES: 
+#version where baselines masquerade as a treatment intensity inside every priority/trt
+
+
+### Libraries -------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+  tidyverse)
+
+### user ---------------------------------------------------
+
+#for the boxplots, free or fixed y axis? 
+box_scales <- "fixed" #"fixed" #free_y
+
+### Results import -------------------------------------------
+
+# can change between region-only and full datacube. 
+# will overwrite
+res_orig <- read_csv(file.path("results",
+                               "absolute", #"datacube", 
+                               "SN_SNbl_absolute_20240416.csv")) %>% 
+  mutate(HUC12 = as.character(HUC12)) 
+
+### Data set up ------------------------------------------------
+
+# Want priority order: 'Fire', 'WUI', 'Hybrid'
+# Want intensity order: '500k', '1m', '2m'
+
+res <- res_orig %>% 
+  #For graphing in the correct order (generic, used in multiple places, with modifications)
+  # make factor with set order (priority)
+  mutate(Priority = as.factor(Priority),
+         Priority = forcats::fct_relevel(Priority,
+                                         "Fire", "WUI", "Hybrid", "baseline", "baseweather"),
+         #Make factor with set order (intensity))
+         TxIntensity = as.factor(TxIntensity),
+         TxIntensity = forcats::fct_relevel(TxIntensity,
+                                            "baseweather", "baseline", "500k", "1m", "2m"),
+         #recode so it fits on graphs
+         TxIntensity = forcats::fct_recode(TxIntensity, "bw" = "baseweather"),
+         TxIntensity = forcats::fct_recode(TxIntensity, "base" = "baseline"))
+
+
+#baseline/baseweather to be treated as an 'intensity' level PER ALL priorities, trts
+res_bases <- res %>% 
+  filter(TxIntensity %in% c("base", "bw")) %>% 
+  #remove priorities, trts from bases
+  dplyr::select(-Priority, -TxType)
+
+#remove bases from rest of results (temporarily)
+res <- res %>% 
+  filter(!TxIntensity %in% c("base", "bw"))
+
+#get all combination of Priority and TxTypes
+frame <- res %>% 
+  dplyr::select(Priority, TxType) %>% 
+  distinct()
+
+#duplicate bases values for all priority-txtype combos
+res_bases <- frame %>% 
+  cross_join(res_bases) 
+
+#add back in
+res <- bind_rows(res, res_bases)
+
+
+year_breaks <- c(2024, 2029, 2034, 2039)
+
+#sampled hucs
+hucs_sample <- read_csv(file.path('qa', 'samplehucs.csv'))
+
+
+
+### Looping version -----------------------------------------------
+
+regions <- res %>% pull(Region) %>% unique()
+
+#REGION (outer loop)
+for (r in seq_along(regions)){
+  
+  this_reg <- regions[[r]]
+  
+  res_r <- res %>% 
+    filter(Region == this_reg)
+  
+  
+  #output 
+  plot_folder <- file.path('plots', 'timeseries_FVS', this_reg)
+  sampled_folder <- file.path(plot_folder, 'sampledhucs')
+  
+  dir.create(sampled_folder, recursive = TRUE)
+  
+  
+  #priorities in this region (all same)
+  priorities <- res_r %>% pull(Priority) %>% unique()
+  
+  #PRIORITY (first inner loop)
+  for (p in seq_along(priorities)){
+    
+    this_priority <- priorities[[p]]
+    
+    res_r_p <- res_r %>% 
+      filter(Priority == this_priority)
+    
+    # get the correct timing group for the different priorities
+    # set as a known field name (to avoid passing field name as variable)
+    if (this_priority == "Fire"){
+      res_r_p <- res_r_p %>% 
+        mutate(timing_group = timeFire) #timeFire fireGrpLbl
+    } else if (this_priority == "WUI"){
+      res_r_p <- res_r_p %>% 
+        mutate(timing_group = timeWui) #timeWui wuiGrpLbl
+    } else if (this_priority == "Hybrid"){
+      res_r_p <- res_r_p %>% 
+        mutate(timing_group = timeHybrid) #timeHybrid hybridGrpLbl
+    # } else if (this_priority %in% c("baseline", "baseweather")){
+    #   res_r_p <- res_r_p %>% 
+    #     mutate(timing_group = "none")
+    } else {
+      stop("Unmatched priority timing group")
+    }
+    
+    #get the correct sampled hucs for this region, priority
+    this_sample <- hucs_sample %>%
+      filter(Region == this_reg,
+             priority == tolower(this_priority))
+
+    res_r_p_sample <- res_r_p %>%
+      filter(HUC12 %in% this_sample$HUC12)
+    
+    #get treatments in this region-priority
+    trts <- res_r_p %>% pull(TxType) %>% unique()
+    
+    
+    #TREATMENT TYPE (second inner loop)
+    for (t in seq_along(trts)){
+      
+      this_trt <- trts[[t]]
+      
+      #get full and sampled datasets for this trt
+      res_r_p_t <- res_r_p %>% 
+        filter(TxType == this_trt)
+      
+      res_r_p_t_sample <- res_r_p_sample %>% 
+        filter(TxType == this_trt)
+      
+      #plot 1 : boxplot all HUCs, fuel_lt3, timeseries
+      p1 <- ggplot() +
+        geom_boxplot(data = res_r_p_t,
+                     mapping = aes(x = Year, y = Fuel_lt3, group=Year)) +
+        scale_x_continuous(breaks = year_breaks) +
+        facet_wrap(~TxIntensity+timing_group, scales = box_scales) +
+        labs(title = paste(this_reg,
+                           this_priority,
+                           this_trt))
+
+      fn1 <- paste0(this_reg, '_', this_priority, '_', this_trt, '_',
+                    'boxplot_', box_scales, '_fuellt3.jpg')
+      ggsave(plot = p1,
+             filename = file.path(plot_folder, fn1),
+             width = 8, height = 8, units = 'in')
+      
+      #plot 2: boxplot all HUCs, fuel 3to6, timeseries
+      p2 <- ggplot() +
+        geom_boxplot(data = res_r_p_t,
+                     mapping = aes(x = Year, y = Fuel_3to6, group=Year)) +
+        scale_x_continuous(breaks = year_breaks) +
+        facet_wrap(~TxIntensity+timing_group, scales = box_scales) +
+        labs(title = paste(this_reg,
+                           this_priority,
+                           this_trt))
+      
+      fn2 <- paste0(this_reg, '_', this_priority, '_', this_trt, '_',
+                    'boxplot_', box_scales, '_fuel3to6.jpg')
+      ggsave(plot = p2,
+             filename = file.path(plot_folder, fn2),
+             width = 8, height = 8, units = 'in')
+      
+
+      #plot 3 : boxplot all HUCs, Fuel 6 to 12, timeseries
+      p3 <- ggplot() +
+        geom_boxplot(data = res_r_p_t,
+                     mapping = aes(x = Year, y = Fuel_6to12, group=Year)) +
+        scale_x_continuous(breaks = year_breaks) +
+        facet_wrap(~TxIntensity+timing_group, scales = box_scales) + 
+        labs(title = paste(this_reg,
+                           this_priority,
+                           this_trt))
+
+      fn3 <- paste0(this_reg, '_', this_priority, '_', this_trt, '_',
+                    'boxplot_', box_scales, '_fuel6to12.jpg')
+      ggsave(plot = p3,
+             filename = file.path(plot_folder, fn3),
+             width = 8, height = 8, units = 'in')
+
+      
+      
+      #second set, selected HUCs only, time series line
+
+      #plot 6: sampled line, fuel less than 3, timeseries
+      p6 <- ggplot(data = res_r_p_t_sample,
+             mapping = aes(x=Year, y=Fuel_lt3, color=TxIntensity)) +
+        geom_jitter(shape = 1, height = 0, width = 0.3) +
+        geom_line() +
+        scale_x_continuous(breaks = year_breaks) +
+        facet_wrap(~paste0(timing_group, " ", HUC12),
+                   ncol = 2, scales='free_y') +
+        labs(title = paste(this_reg,
+                           this_priority,
+                           this_trt,
+                           "Selected HUCs"))
+
+      fn6 <- paste0('sampled_', this_reg, '_', this_priority, '_', this_trt, '_',
+                    'line_freey_', 'fuellt3.jpg')
+      ggsave(plot = p6,
+             filename = file.path(sampled_folder, fn6),
+             width = 7, height = 7, units = 'in')
+
+      #plot 7: samples line, fuel 3 to 6, timeseries
+      p7 <- ggplot(data = res_r_p_t_sample,
+                   mapping = aes(x=Year, y=Fuel_3to6, color=TxIntensity)) +
+        geom_jitter(shape = 1, height = 0, width = 0.3) +
+        geom_line() +
+        scale_x_continuous(breaks = year_breaks) +
+        facet_wrap(~paste0(timing_group, " ", HUC12),
+                   ncol = 2, scales='free_y') +
+        labs(title = paste(this_reg,
+                           this_priority,
+                           this_trt,
+                           "Selected HUCs"))
+      
+      fn7 <- paste0('sampled_', this_reg, '_', this_priority, '_', this_trt, '_',
+                    'line_freey_', 'fuel3to6.jpg')
+      ggsave(plot = p7,
+             filename = file.path(sampled_folder, fn7),
+             width = 7, height = 7, units = 'in')
+      
+
+      #plot 8: sampled line, Fuel 6 to 12, timeseries
+      p8 <- ggplot(data = res_r_p_t_sample,
+             mapping = aes(x=Year, y=Fuel_6to12, color=TxIntensity)) +
+        geom_jitter(shape = 1, height = 0, width = 0.3) +
+        geom_line() +
+        scale_x_continuous(breaks = year_breaks) +
+        facet_wrap(~paste0(timing_group, " ", HUC12),
+                   ncol = 2, scales='free_y') +
+        labs(title = paste(this_reg,
+                           this_priority,
+                           this_trt,
+                           "Selected HUCs"))
+
+      fn8 <- paste0('sampled_', this_reg, '_', this_priority, '_', this_trt, '_',
+                    'line_freey_', 'fuel6to12.jpg')
+      ggsave(plot = p8,
+             filename = file.path(sampled_folder, fn8),
+             width = 7, height = 7, units = 'in')
+
+      
+    } # end t trt
+    
+  } # end p priorities
+  
+} #end r regions
+  
+
+
