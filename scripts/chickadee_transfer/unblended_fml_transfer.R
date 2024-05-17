7b1489514fb9756969d7a53fd776927701f8a9cf
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2024-05-17 12:59:00 -0700

    Rework how unblended rasters are identified (switch to creating targets instead of scanning)

--- scripts/chickadee_transfer/unblended_fml_transfer.R
+++ scripts/chickadee_transfer/unblended_fml_transfer.R
@@ -8,44 +8,84 @@ pacman::p_load(tidyverse)
 
 ### User settings ---------------------------------------------
 
-#Mounted shared: R:\
-shared <- file.path("R:", "rem")
-#test <- file.path("R:", "rem", "RunID19_SC_WUI_500k_trt1")
-
 #local
 outfolder <- file.path(
   #backslashes escaped
   "\\\\tsclient", 
   "C", "Users", "nekodawn", "code_local", 
-  "huc_model_fire", "data", "fuels_merged_bluejay")
+  "huc_model_fire", "qaqc_fvs", "nc_qaqc") #sc_qaqc, cc_qaqc, nc_qaqc, sn_qaqc
 
 # run ids, include _, important for single digit runids
-selected_runs <- "RunID10_|RunID16_|RunID19_|RunID25_|RunID28_|RunID34_"
+selected_list <- 
+  c("RunID10_NC_WUI_500k_trt7", "RunID15_NC_Fire_1m_trt7") 
+selected_runs <- paste(selected_list, collapse="|")
 
+#"RunID10_|RunID16_|RunID19_|RunID25_|RunID28_|RunID34_"
 #"RunID9_|RunID12_|RunID18_|RunID21_|RunID27_|RunID30_|RunID36_"
+
+
+#Mounted shared: R:\
+shared <- file.path("R:", "rem")
+
 
 (time_start <- Sys.time())
 
 
 ### select files -----------------------------------------------------
 
+#original fvs
+fvs_names_orig <- read_csv(file.path("data", "FvsNames.csv"))
+
+#create NC trt7 runids. 
+fvs_nc7 <- fvs_names_orig %>% 
+  filter(grepl('NC.+trt4', name)) %>% 
+  mutate(name = str_replace(name, 'trt4', 'trt7'))
+#remove NC trt4, add in trt7
+fvs_names_all <- fvs_names_orig %>% 
+  #! is not
+  filter(!grepl('NC.+trt4', name)) %>% 
+  bind_rows(fvs_nc7) 
+
+fvs_names_all <- fvs_names_all %>% 
+  mutate(merged_folders = file.path('R:',
+                                    'rem',
+                                    name,
+                                    "FVS_fuels",
+                                    "outputs"))
+
+selected_rem <- grep(pattern=selected_runs, 
+                     fvs_names_all %>% pull(merged_folders),
+                     value=TRUE)
+
+
+# files_to_get <- list.files(selected_rem,
+#                            full.names = TRUE,
+#                            recursive = TRUE,
+#                            pattern = "RunID.+_fml_merged.tif$")
+
+files_to_get <- list.files(selected_rem,
+                           full.names = TRUE,
+                           recursive = TRUE,
+                           pattern = "RunID.+_cc_merged.tif$")
+
+
 # No pattern parameter in list.dirs unlike list.files
 # Plan is to get all directories, then filter to correct directories
 # Then copy files over
 
-all_dirs <- list.dirs(shared)
-#RunIDs
-run_dirs <- grep(selected_runs, all_dirs, value=TRUE)
-#just FVS_fuels
-fvsfuels_dirs <- grep("FVS_fuels", run_dirs, value=TRUE)
-#just blended fuels
-blended_dirs <- grep("outputs", fvsfuels_dirs, value=TRUE)
-
-
-files_to_get <- list.files(blended_dirs,
-                           full.names = TRUE,
-                           recursive = TRUE,
-                           pattern = "RunID.+_fml_merged.tif$")
+# all_dirs <- list.dirs(shared)
+# #RunIDs
+# run_dirs <- grep(selected_runs, all_dirs, value=TRUE)
+# #just FVS_fuels
+# fvsfuels_dirs <- grep("FVS_fuels", run_dirs, value=TRUE)
+# #just blended fuels
+# blended_dirs <- grep("outputs", fvsfuels_dirs, value=TRUE)
+# 
+# 
+# files_to_get <- list.files(blended_dirs,
+#                            full.names = TRUE,
+#                            recursive = TRUE,
+#                            pattern = "RunID.+_fml_merged.tif$")
 
 
 ### copy files --------------------------------------------------------
