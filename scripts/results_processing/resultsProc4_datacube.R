bd30d9193ceaedbf56ea5cc5b2648995c0a90192
Dawn Nekorchuk <28901045+michdn@users.noreply.github.com>
2024-06-20 16:01:54 -0700

    Add vegetation to cube

--- scripts/results_processing/resultsProc4_datacube.R
+++ scripts/results_processing/resultsProc4_datacube.R
@@ -1,6 +1,6 @@
 # Short script to bind all region absolute metrics together for final datacube
 
-
+# Also adds veg proportion (from Anna)
 
 
 ### Libraries -------------------------------------------------
@@ -18,22 +18,42 @@ dir.create(output_folder, recursive = TRUE)
 
 
 #region results
-sn <- read_csv(file.path(input_folder, "SN_absolute_202405xx.csv")) %>% 
+sn <- read_csv(file.path(input_folder, "SN_absolute_20240617.csv")) %>% 
   mutate(HUC12 = as.character(HUC12))
 
-sc <- read_csv(file.path(input_folder, "SC_absolute_202406xx.csv")) %>% 
+sc <- read_csv(file.path(input_folder, "SC_absolute_20240617.csv")) %>% 
   mutate(HUC12 = as.character(HUC12))
 
-cc <- read_csv(file.path(input_folder, "CC_absolute_202406xx.csv")) %>% 
+cc <- read_csv(file.path(input_folder, "CC_absolute_20240617.csv")) %>% 
 mutate(HUC12 = as.character(HUC12))
 
-nc <- read_csv(file.path(input_folder, "NC_absolute_202406xx.csv")) %>% 
-  mutate(HUC12 = as.character(HUC12))
+# nc <- read_csv(file.path(input_folder, "NC_absolute_202406xx.csv")) %>% 
+#   mutate(HUC12 = as.character(HUC12))
+
+# Vegetation proportions
+veg <- read_csv(file.path("data", "data_veg", 
+                          "PropVegTypeForDatacube.csv")) %>% 
+  rename(HUC12 = huc12) %>% 
+  mutate(HUC12 = as.character(HUC12)) 
 
 
-### Bind and save -------------------------------------------
 
-cube <- bind_rows(sn, sc, cc, nc)
+### Bind and add veg -------------------------------------------
+
+cube_raw <- bind_rows(sn, sc, cc) #, nc
+
+cube <- cube_raw %>% 
+  left_join(veg, 
+            by = join_by(HUC12)) %>% 
+  #reorder
+  dplyr::select(HUC12, Region, Priority, TxIntensity, TxType, Year, 
+                mas_scenario, hucAc, timeFire, timeHybrid, timeWui,
+                hardwoodSum, herbSum, shrubSum, softwoodSum,
+                vegSum, propHard, propSoft, propShrub, propHerb,
+                everything())
+
+
+### save ---------------------------------------------------
 
 stamp <- format(Sys.time(), "%Y%m%d")
 
